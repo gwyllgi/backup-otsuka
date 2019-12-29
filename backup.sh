94fc1da506ff852f7a41b83e3e57ac46c0d83dc3
gwyllgi <gwyllgi.dollars@gmail.com>
2019-12-29 11:20:28 -0500

    backupotsuka

---
+++ backup.sh
@@ -0,0 +1,22 @@
+#Folder yang akan dibackup.
+backup_files="/var/www"
+
+#Output backup.
+dest"/home"
+day=$(date +%Y-%m-%d)
+archive_file="otsuka-backup-$day.tar.gz"
+
+#Print status pesan.
+echo "Proses backup $backup_files ke $dest/$archive_file"
+date
+echo
+
+#Backup files dengan tar.
+tar czf $dest/$archive_file $backup_files
+
+#Print status selesai
+echo
+echo "Backup Selesai"
+
+#Listing files check
+ls -lh $dest
